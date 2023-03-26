2b2a7a92130d355aae1387ca4c12ce90064393aa
hwh <hwh@qq.com>
2023-03-26 22:25:22 +0800

    23-03-26

--- src/com/hwh/leet/Climb_Stairs_15.java
+++ src/com/hwh/leet/Climb_Stairs_15.java
@@ -3,6 +3,7 @@ package com.hwh.leet;
 /**
  * @author hwh
  * @create 2023-03-26 21:41
+ * 最后一步只可能是一步或两步，那么剩下的可能就是f(n-1)+f(n-2)
  */
 public class Climb_Stairs_15 {
     public static void main(String[] args) {
