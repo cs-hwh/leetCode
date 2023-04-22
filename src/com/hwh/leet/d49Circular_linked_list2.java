bf50beef09b2a5b68a803ee7d499b0c2c275077c
hwh <hwh@qq.com>
2023-04-22 22:48:28 +0800

    23-04-22

---
+++ src/com/hwh/leet/d49Circular_linked_list2.java
@@ -0,0 +1,20 @@
+package com.hwh.leet;
+
+import com.hwh.leet.util.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author hwh
+ * @create 2023-04-22 21:14
+ */
+public class d49Circular_linked_list2 {
+    public static void main(String[] args) {
+
+    }
+    public ListNode detectCycle(ListNode head) {
+
+      return null;
+    }
+}
